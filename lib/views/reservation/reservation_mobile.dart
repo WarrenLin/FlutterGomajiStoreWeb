8958a6deeafb0037bcaf9589f47890c5a2541b16
Warren <warren@gomaji.com>
2020-03-19 12:20:55 +0800

    1. Remove status bar height.

--- lib/views/reservation/reservation_mobile.dart
+++ lib/views/reservation/reservation_mobile.dart
@@ -8,63 +8,66 @@ class ReservationMobile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height:
-          MediaQuery.of(context).size.height - AppBar().preferredSize.height - 24,
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
-                child: Column(
-                  children: <Widget>[
-                    FilterView(
-                      width: MediaQuery.of(context).size.width,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    SearchView(
-                      width: MediaQuery.of(context).size.width,
-                    ),
-                  ],
+          MediaQuery.of(context).size.height - AppBar().preferredSize.height,
+      child: Scrollbar(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  child: Column(
+                    children: <Widget>[
+                      FilterView(
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      SearchView(
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              color: Colors.black12,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      "2020/3/2",
-                      style: TextStyle(color: Colors.redAccent, fontSize: 20),
-                    ),
-                    Text(
-                      "總共4筆預約",
-                      style: TextStyle(color: Colors.blueAccent, fontSize: 18),
-                    ),
-                    ReservationItemView(
-                        "林老師", "2020/3/2 10:30~11:00", "使用張數:1張 泰SPA 古法全身精油舒壓"),
-                    ReservationItemView(
-                        "李小龍", "2020/3/2 10:30~11:00", "使用張數:2張 泰SPA 古法全身精油舒壓"),
-                    ReservationItemView("喇叭詹", "2020/3/2 10:30~11:00",
-                        "使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓"),
-                    ReservationItemView(
-                        "林老師", "2020/3/2 10:30~11:00", "使用張數:1張 泰SPA 古法全身精油舒壓"),
-                    ReservationItemView(
-                        "林老師", "2020/3/2 10:30~11:00", "使用張數:1張 泰SPA 古法全身精油舒壓"),
-                    ReservationItemView("喇叭詹", "2020/3/2 10:30~11:00",
-                        "使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓"),
-                  ],
+              Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.black12,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        "2020/4/2",
+                        style: TextStyle(color: Colors.redAccent, fontSize: 20),
+                      ),
+                      Text(
+                        "總共4筆預約",
+                        style:
+                            TextStyle(color: Colors.blueAccent, fontSize: 18),
+                      ),
+                      ReservationItemView("林老師", "2020/3/2 10:30~11:00",
+                          "使用張數:1張 泰SPA 古法全身精油舒壓"),
+                      ReservationItemView("李小龍", "2020/3/2 10:30~11:00",
+                          "使用張數:2張 泰SPA 古法全身精油舒壓"),
+                      ReservationItemView("喇叭詹", "2020/3/2 10:30~11:00",
+                          "使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓"),
+                      ReservationItemView("林老師", "2020/3/2 10:30~11:00",
+                          "使用張數:1張 泰SPA 古法全身精油舒壓"),
+                      ReservationItemView("林老師", "2020/3/2 10:30~11:00",
+                          "使用張數:1張 泰SPA 古法全身精油舒壓"),
+                      ReservationItemView("喇叭詹", "2020/3/2 10:30~11:00",
+                          "使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓, 使用張數:3張 泰SPA 古法全身精油舒壓"),
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
